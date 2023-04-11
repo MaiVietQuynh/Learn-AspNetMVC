91a237b3d809c76ab4d1acaf318542001546d710
MaiVietQuynh <maivietquynh2001@gmail.com>
2023-04-11 15:53:22 +0700

    Add project files.

---
+++ Learn-AspNetMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Learn_AspNetMVC.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
